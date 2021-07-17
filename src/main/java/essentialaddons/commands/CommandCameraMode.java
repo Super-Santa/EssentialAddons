289820e9ac89919f1ebe844505e74f1e92cc3210
senseiwells <wellsm488@gmail.com>
2021-07-17 13:06:05 +0100

    overhauled cs

--- src/main/java/essentialaddons/commands/CommandCameraMode.java
+++ src/main/java/essentialaddons/commands/CommandCameraMode.java
@@ -1,176 +1,66 @@
 package essentialaddons.commands;
 
-import essentialaddons.EssentialAddonsSettings;
-import essentialaddons.EssentialAddonsUtils;
 import carpet.settings.SettingsManager;
 import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.mob.HostileEntity;
+import essentialaddons.EssentialAddonsServer;
+import essentialaddons.EssentialAddonsSettings;
+import essentialaddons.utils.CameraData;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Box;
 import net.minecraft.world.GameMode;
-import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class CommandCameraMode {
 
+    private static final Logger LOGGER = LogManager.getLogger("EssentialAddons|CameraData");
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("cs").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandCameraMode)
-        ).executes(context -> {
-            //variables
-            ServerPlayerEntity playerEntity = context.getSource().getPlayer();
-            UUID playerUUID = playerEntity.getUuid();
-            ServerWorld overworld = context.getSource().getMinecraftServer().getWorld(World.OVERWORLD);
-            ServerWorld nether = context.getSource().getMinecraftServer().getWorld(World.NETHER);
-            ServerWorld end = context.getSource().getMinecraftServer().getWorld(World.END);
-            if (!playerEntity.isSpectator()){
-                //checks for cameraModeRestoreLocation
-                if (!SettingsManager.canUseCommand(null, EssentialAddonsSettings.cameraModeRestoreLocation)) {
-                    playerEntity.setGameMode(GameMode.SPECTATOR);
-                    EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been put into §aSPECTATOR");
+                ).executes(context -> {
+                    toggle(context.getSource().getPlayer());
                     return 0;
-                }
-                //checks for survivalRestrictions carpet rule and whether the player is in danger
-                if (SettingsManager.canUseCommand(null, EssentialAddonsSettings.cameraModeSurvivalRestrictions) && playerEntity.interactionManager.getGameMode() == GameMode.SURVIVAL && isInDanger(playerEntity))
-                    return 0;
-                //sets location to HashMap and File
-                setLocationToHashMap(playerEntity, playerUUID, overworld, nether);
-                setLocationToFile(playerEntity);
+                })
+        );
+    }
+    private static void cameraMode(ServerPlayerEntity playerEntity) {
+        if (EssentialAddonsSettings.cameraModeRestoreLocation) {
+            EssentialAddonsServer.cameraData.put(playerEntity.getUuid(), new CameraData(playerEntity));
+            try {
+                CameraData.writeSaveFile(EssentialAddonsServer.cameraData);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            //checks for cameraModeSurvivalRestrictions
-            else if (playerEntity.isSpectator() && !SettingsManager.canUseCommand(null, EssentialAddonsSettings.cameraModeRestoreLocation)) {
-                playerEntity.setGameMode(playerEntity.interactionManager.getPreviousGameMode());
-                EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been put into §a" + playerEntity.interactionManager.getPreviousGameMode());
+        }
+        playerEntity.setGameMode(GameMode.SPECTATOR);
+    }
+    private static void returnMode(ServerPlayerEntity playerEntity) {
+        CameraData data = EssentialAddonsServer.cameraData.remove(playerEntity.getUuid());
+        if (data == null) {
+            try {
+                EssentialAddonsServer.cameraData = CameraData.readSaveFile();
+                data = EssentialAddonsServer.cameraData.remove(playerEntity.getUuid());
             }
-                //checks if hashmap has location data before accessing file for data
-            else if (playerEntity.isSpectator() && dimension.get(playerUUID) != null && x.get(playerUUID) != null && y.get(playerUUID) != null && z.get(playerUUID) != null && yaw.get(playerUUID) != null && pitch.get(playerUUID) != null)
-                returnToPreviousGamemode(playerEntity, playerUUID);
-            //if hashmap is empty then it will try and get data from file
-            else if (playerEntity.isSpectator()) {
-                getLocationFromFile(playerEntity, playerUUID, overworld, nether, end);
-                returnToPreviousGamemode(playerEntity, playerUUID);
+            catch (IOException e) {
+                e.printStackTrace();
             }
-            else {
-                //this should never happen
-                EssentialAddonsUtils.sendToActionBar(playerEntity, "§c[ERROR] Something went very wrong");
-            }
-            return 0;
-        }));
+        }
+        if (EssentialAddonsSettings.cameraModeRestoreLocation && data != null)
+            data.restore(playerEntity);
+        else if (EssentialAddonsSettings.cameraModeRestoreLocation)
+            LOGGER.error("Could not load previous location");
+        playerEntity.setGameMode(playerEntity.interactionManager.getPreviousGameMode());
     }
-    //unchecked cast warning
-    @SuppressWarnings("unchecked")
-    private static void getLocationFromFile(ServerPlayerEntity playerEntity, UUID playerUUID, ServerWorld overworld, ServerWorld nether, ServerWorld end) {
-        try {
-            EssentialAddonsUtils.directoryExists("world/playerdata/cs");
-            FileInputStream fis = new FileInputStream("world/playerdata/cs/" + playerUUID + ".cs");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            dim = (HashMap<UUID, String>) ois.readObject();
-            x = (HashMap<UUID, Double>) ois.readObject();
-            y = (HashMap<UUID, Double>) ois.readObject();
-            z = (HashMap<UUID, Double>) ois.readObject();
-            yaw = (HashMap<UUID, Float>) ois.readObject();
-            pitch = (HashMap<UUID, Float>) ois.readObject();
-            ois.close();
-            fis.close();
+    private static void toggle(ServerPlayerEntity playerEntity) {
+        if (playerEntity.isSpectator()) {
+            returnMode(playerEntity);
         }
-        //this should only happen if player was previously in spectator before rule was enabled
-        catch (IOException | ClassNotFoundException e) {
-            System.out.println("getLocationFromFileCatch");
-            e.printStackTrace();
-            EssentialAddonsUtils.sendToActionBar(playerEntity, "§c[ERROR] Unable to get survival location");
-            playerEntity.setGameMode(GameMode.SURVIVAL);
-        }
-        //this just double checks to see if HashMap is empty, error can occur when other HashMaps are saved and it tries to read an non-existent HashMap
-        if (dim.get(playerUUID) == null || x.get(playerUUID) == null || y.get(playerUUID) == null || z.get(playerUUID) == null || yaw.get(playerUUID) == null || pitch.get(playerUUID) == null){
-            EssentialAddonsUtils.sendToActionBar(playerEntity, "§c[ERROR] Unable to get survival location");
-            playerEntity.setGameMode(GameMode.SURVIVAL);
-        }
-        //decoding the dimension data from earlier
-        if (dim.get(playerUUID).equals("overworld"))
-            dimension.put(playerUUID, overworld);
-        else if (dim.get(playerUUID).equals("nether"))
-            dimension.put(playerUUID, nether);
-        else
-            dimension.put(playerUUID, end);
-    }
-    private static void setLocationToFile(ServerPlayerEntity playerEntity) {
-        try {
-            EssentialAddonsUtils.directoryExists("world/playerdata/cs");
-            //serialises and saves HashMap into a file
-            FileOutputStream fos = new FileOutputStream("world/playerdata/cs/" + playerEntity.getUuid() + ".cs");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(dim);
-            oos.writeObject(x);
-            oos.writeObject(y);
-            oos.writeObject(z);
-            oos.writeObject(yaw);
-            oos.writeObject(pitch);
-            oos.close();
-            fos.close();
-            //sets player gamemode to spectator
-            playerEntity.setGameMode(GameMode.SPECTATOR);
-            EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been put into §aSPECTATOR");
-        }
-        catch (IOException ioe) {
-            ioe.printStackTrace();
-            EssentialAddonsUtils.sendToActionBar(playerEntity, "§c[ERROR] Unable to save survival location");
+        else {
+            cameraMode(playerEntity);
         }
     }
-    private static void setLocationToHashMap(ServerPlayerEntity playerEntity, UUID playerUUID, ServerWorld overworld, ServerWorld nether) {
-        //gets the location of the player and puts it in a hashmap
-        dimension.put(playerUUID,playerEntity.getServerWorld());
-        x.put(playerUUID,playerEntity.getX());
-        y.put(playerUUID,playerEntity.getY());
-        z.put(playerUUID,playerEntity.getZ());
-        yaw.put(playerUUID,playerEntity.getYaw(1));
-        pitch.put(playerUUID,playerEntity.getPitch(1));
-        //converting the ServerWorld to a string because it doesn't serialise (probably a much better way of doing this)
-        if (playerEntity.getServerWorld() == overworld)
-            dim.put(playerUUID, "overworld");
-        else if (playerEntity.getServerWorld() == nether)
-            dim.put(playerUUID, "nether");
-        else
-            dim.put(playerUUID, "end");
-    }
-    private static void returnToPreviousGamemode(ServerPlayerEntity playerEntity, UUID playerUUID) {
-        GameMode previousGameMode = playerEntity.interactionManager.getPreviousGameMode();
-        playerEntity.teleport(dimension.get(playerUUID),x.get(playerUUID),y.get(playerUUID),z.get(playerUUID),yaw.get(playerUUID),pitch.get(playerUUID));
-        playerEntity.setGameMode(previousGameMode);
-        EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been put into §a" + previousGameMode);
-    }
-    private static boolean isInDanger(ServerPlayerEntity playerEntity) {
-        Box nearPlayer = new Box(playerEntity.getX()-4,playerEntity.getY()-4,playerEntity.getZ()-4,playerEntity.getX()+4,playerEntity.getY()+4,playerEntity.getZ()+4);
-        List<HostileEntity> list = playerEntity.world.getEntitiesByClass(HostileEntity.class, nearPlayer, hostileEntity -> true);
-        String reason;
-        if (playerEntity.isOnFire())
-            reason = "you are on fire";
-        else if (playerEntity.hasStatusEffect(StatusEffects.POISON) || playerEntity.hasStatusEffect(StatusEffects.WITHER) || playerEntity.hasStatusEffect(StatusEffects.LEVITATION))
-            reason = "you have a negative status effect";
-        else if (playerEntity.fallDistance > 0)
-            reason = "you are falling";
-        else if (playerEntity.isFallFlying())
-            reason = "you are flying";
-        else if (!list.isEmpty())
-            reason = "there are mobs nearby";
-        else
-            return false;
-        EssentialAddonsUtils.sendToActionBar(playerEntity, "§cYou cannot enter spectator because " + reason);
-        return true;
-    }
-    //Hashmaps that store player locations
-    private static final HashMap<UUID, ServerWorld> dimension = new HashMap<>();
-    private static HashMap<UUID, String> dim = new HashMap<>();
-    private static HashMap<UUID, Double> x = new HashMap<>();
-    private static HashMap<UUID, Double> y = new HashMap<>();
-    private static HashMap<UUID, Double> z = new HashMap<>();
-    private static HashMap<UUID, Float> yaw = new HashMap<>();
-    private static HashMap<UUID, Float> pitch = new HashMap<>();
 }
---
+++ src/main/java/essentialaddons/utils/CameraData.java
@@ -0,0 +1,95 @@
+package essentialaddons.utils;
+
+import carpet.CarpetServer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.WorldSavePath;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class CameraData {
+    public static final MapCodec<CameraData> CODEC = RecordCodecBuilder.mapCodec(it -> it.group(
+            Identifier.CODEC.fieldOf("dimension").forGetter(d -> d.dimension.getValue()),
+            Codec.DOUBLE.fieldOf("x").forGetter(d -> d.position.x),
+            Codec.DOUBLE.fieldOf("y").forGetter(d -> d.position.y),
+            Codec.DOUBLE.fieldOf("z").forGetter(d -> d.position.z),
+            Codec.FLOAT.fieldOf("yaw").forGetter(d -> d.yaw),
+            Codec.FLOAT.fieldOf("pitch").forGetter(d -> d.pitch)
+    ).apply(it, (dim, x, y, z, yaw, pitch) -> new CameraData(RegistryKey.of(Registry.DIMENSION, dim), new Vec3d(x, y, z), yaw, pitch)));
+
+    public static final Codec<Map<UUID, CameraData>> MAP_CODEC = Codec.unboundedMap(Codec.STRING.xmap(UUID::fromString, UUID::toString), CODEC.codec());
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    private static final Logger LOGGER = LogManager.getLogger("EssentialAddons|CameraData");
+
+    public final @Nonnull RegistryKey<World> dimension;
+    public final @Nonnull Vec3d position;
+    public final float yaw;
+    public final float pitch;
+
+    public CameraData(@Nonnull RegistryKey<World> dimension, @Nonnull Vec3d position, float yaw, float pitch) {
+        this.dimension = dimension;
+        this.position = position;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    public CameraData(Entity entity) {
+        this(entity.world.getRegistryKey(), entity.getPos(), entity.getYaw(1), entity.getPitch(1));
+    }
+
+    public void restore(ServerPlayerEntity playerEntity) {
+        MinecraftServer server = playerEntity.world.getServer();
+        if (server == null) return;
+        ServerWorld world = server.getWorld(dimension);
+        playerEntity.teleport(world, position.x, position.y, position.z, yaw, pitch);
+    }
+
+    private static Path getFile() {
+        return CarpetServer.minecraft_server.getSavePath(WorldSavePath.ROOT).resolve("cameradata.json");
+    }
+    public static Map<UUID, CameraData> readSaveFile() throws IOException {
+        Path file = getFile();
+        if (!Files.isRegularFile(file)) return new HashMap<>();
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            return new HashMap<>(MAP_CODEC.decode(JsonOps.INSTANCE, JsonHelper.deserialize(reader))
+                    .getOrThrow(false, e -> LOGGER.error("Could not read camera data: {}", e))
+                    .getFirst());
+        }
+    }
+    public static void writeSaveFile(Map<UUID, CameraData> data) throws IOException {
+        Path file = getFile();
+        if (data.isEmpty()) {
+            Files.deleteIfExists(file);
+            return;
+        }
+        try(BufferedWriter writer = Files.newBufferedWriter(file)) {
+            MAP_CODEC.encodeStart(JsonOps.INSTANCE, data)
+                    .resultOrPartial(e -> LOGGER.error("Could not write camera data: {}", e))
+                    .ifPresent(obj -> GSON.toJson(obj, writer));
+        }
+    }
+}
