942e339ec6ed8fb1be5a2674d8fb4a7b8c2401a8
senseiwells <wellsm488@gmail.com>
2021-07-21 00:03:34 +0100

    fixed bug

--- src/main/java/essentialaddons/mixins/LivingEntityMixin.java
+++ src/main/java/essentialaddons/mixins/LivingEntityMixin.java
@@ -17,8 +17,9 @@ public class LivingEntityMixin {
     @Redirect(method = "eatFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"), require = 0)
     private void onDecrement(ItemStack itemStack, int amount, World world) {
         if (EssentialAddonsSettings.infiniteItems) {
-            if (!((LivingEntity) (Object) this instanceof PlayerEntity)) {
+            if (((LivingEntity) (Object) this instanceof PlayerEntity) == false) {
                 itemStack.decrement(1);
+                return;
             }
             if (!world.isClient) {
                 ServerPlayerEntity playerEntity = (ServerPlayerEntity) (Object) this;
