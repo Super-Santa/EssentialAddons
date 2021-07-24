c602ee337588eb45578ac3d7a35e79656d365bf5
senseiwells <wellsm488@gmail.com>
2021-07-24 13:22:16 +0100

    fixed NullPointerException in `essentialCarefulBreak`

--- src/main/java/essentialaddons/mixins/essentialCarefulbreak/BlockMixin.java
+++ src/main/java/essentialaddons/mixins/essentialCarefulbreak/BlockMixin.java
@@ -17,28 +17,45 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.IOException;
+
 @Mixin(Block.class)
 public abstract class BlockMixin {
     //Code from wholmT
     @Redirect(method = "afterBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;dropStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)V"), require = 0)
-    private void onDropStacks(BlockState state, World world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack){
-        if (EssentialAddonsSettings.essentialCarefulBreak && entity instanceof PlayerEntity && entity.isInSneakingPose() && SubscribeData.subscribeData.get(entity.getUuid()).isSubscribedCarefulBreak)
-            EssentialAddonsUtils.placeItemInInventory(state,world,pos,blockEntity,entity,stack);
-        else
-            Block.dropStacks(state,world,pos,blockEntity,entity,stack);
+    private void onDropStacks(BlockState state, World world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack) throws IOException {
+        if (EssentialAddonsSettings.essentialCarefulBreak && entity instanceof PlayerEntity && entity.isInSneakingPose()) {
+            SubscribeData data = SubscribeData.subscribeData.get(entity.getUuid());
+            if (data == null) {
+                SubscribeData.subscribeData.put(entity.getUuid(), new SubscribeData(false, false));
+                SubscribeData.writeSaveFile(SubscribeData.subscribeData);
+            }
+            else if (data.isSubscribedCarefulBreak) {
+                EssentialAddonsUtils.placeItemInInventory(state, world, pos, blockEntity, entity, stack);
+                return;
+            }
+        }
+        Block.dropStacks(state, world, pos, blockEntity, entity, stack);
     }
     //carefulBreak PISTON_HEADS
     @Inject(method = "onBreak", at = @At("HEAD"))
-    private void onBreak1(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
-        if (EssentialAddonsSettings.essentialCarefulBreak && player.isInSneakingPose() && SubscribeData.subscribeData.get(player.getUuid()).isSubscribedCarefulBreak) {
-            if (state.getBlock() == Blocks.PISTON_HEAD) {
-                Direction direction = state.get(FacingBlock.FACING).getOpposite();
-                pos = pos.offset(direction);
-                BlockState blockState = world.getBlockState(pos);
-                Block block = world.getBlockState(pos).getBlock();
-                if (block == Blocks.PISTON || block == Blocks.STICKY_PISTON && blockState.get(PistonBlock.EXTENDED)) {
-                    EssentialAddonsUtils.placeItemInInventory(blockState, world, pos, null, player, player.getMainHandStack());
-                    world.removeBlock(pos, false);
+    private void onBreak1(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) throws IOException {
+        if (EssentialAddonsSettings.essentialCarefulBreak && player.isInSneakingPose()) {
+            SubscribeData data = SubscribeData.subscribeData.get(player.getUuid());
+            if (data == null) {
+                SubscribeData.subscribeData.put(player.getUuid(), new SubscribeData(false, false));
+                SubscribeData.writeSaveFile(SubscribeData.subscribeData);
+            }
+            else if (data.isSubscribedCarefulBreak) {
+                if (state.getBlock() == Blocks.PISTON_HEAD) {
+                    Direction direction = state.get(FacingBlock.FACING).getOpposite();
+                    pos = pos.offset(direction);
+                    BlockState blockState = world.getBlockState(pos);
+                    Block block = world.getBlockState(pos).getBlock();
+                    if (block == Blocks.PISTON || block == Blocks.STICKY_PISTON && blockState.get(PistonBlock.EXTENDED)) {
+                        EssentialAddonsUtils.placeItemInInventory(blockState, world, pos, null, player, player.getMainHandStack());
+                        world.removeBlock(pos, false);
+                    }
                 }
             }
         }
