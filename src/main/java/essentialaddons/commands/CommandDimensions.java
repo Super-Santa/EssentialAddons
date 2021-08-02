11fd972b17cd8b41ff4d97b253166194d983917a
senseiwells <wellsm488@gmail.com>
2021-08-02 11:40:33 +0100

    added arguments for `commandDimensions`

--- src/main/java/essentialaddons/commands/CommandDimensions.java
+++ src/main/java/essentialaddons/commands/CommandDimensions.java
@@ -2,6 +2,7 @@ package essentialaddons.commands;
 
 import carpet.settings.SettingsManager;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import essentialaddons.EssentialAddonsSettings;
 import essentialaddons.EssentialAddonsUtils;
 import net.minecraft.server.command.ServerCommandSource;
@@ -9,6 +10,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 
+import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class CommandDimensions {
@@ -19,24 +21,67 @@ public class CommandDimensions {
                     ServerWorld world = context.getSource().getServer().getWorld(World.OVERWORLD);
                     toDimension(playerEntity, world, "OVERWORLD");
                     return 0;
-                }));
+                })
+                .then(argument("x", IntegerArgumentType.integer())
+                        .then(argument("y", IntegerArgumentType.integer())
+                                .then(argument("z", IntegerArgumentType.integer())
+                                        .executes(context -> {
+                                            ServerPlayerEntity playerEntity = context.getSource().getPlayer();
+                                            ServerWorld world = context.getSource().getServer().getWorld(World.OVERWORLD);
+                                            toDimension(playerEntity, world, "OVERWORLD", context.getArgument("x", Integer.class), context.getArgument("y", Integer.class), context.getArgument("z", Integer.class));
+                                            return 0;
+                                        })
+                                )
+                        )
+                )
+        );
         dispatcher.register(literal("nether").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandDimensions))
                 .executes(context -> {
                     ServerPlayerEntity playerEntity = context.getSource().getPlayer();
                     ServerWorld world = context.getSource().getServer().getWorld(World.NETHER);
                     toDimension(playerEntity, world, "NETHER");
                     return 0;
-                }));
+                })
+                .then(argument("x", IntegerArgumentType.integer())
+                        .then(argument("y", IntegerArgumentType.integer())
+                                .then(argument("z", IntegerArgumentType.integer())
+                                        .executes(context -> {
+                                            ServerPlayerEntity playerEntity = context.getSource().getPlayer();
+                                            ServerWorld world = context.getSource().getServer().getWorld(World.NETHER);
+                                            toDimension(playerEntity, world, "NETHER", context.getArgument("x", Integer.class), context.getArgument("y", Integer.class), context.getArgument("z", Integer.class));
+                                            return 0;
+                                        })
+                                )
+                        )
+                )
+        );
         dispatcher.register(literal("end").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandDimensions))
                 .executes(context -> {
                     ServerPlayerEntity playerEntity = context.getSource().getPlayer();
                     ServerWorld world = context.getSource().getServer().getWorld(World.END);
                     toDimension(playerEntity, world, "END");
                     return 0;
-                }));
+                })
+                .then(argument("x", IntegerArgumentType.integer())
+                        .then(argument("y", IntegerArgumentType.integer())
+                                .then(argument("z", IntegerArgumentType.integer())
+                                        .executes(context -> {
+                                            ServerPlayerEntity playerEntity = context.getSource().getPlayer();
+                                            ServerWorld world = context.getSource().getServer().getWorld(World.END);
+                                            toDimension(playerEntity, world, "END", context.getArgument("x", Integer.class), context.getArgument("y", Integer.class), context.getArgument("z", Integer.class));
+                                            return 0;
+                                        })
+                                )
+                        )
+                )
+        );
     }
     private static void toDimension(ServerPlayerEntity playerEntity, ServerWorld world, String dimension) {
         playerEntity.teleport(world, 0, 128, 0, 0, 0);
         EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been teleported to 0,0 in the §a" + dimension);
+    }
+    private static void toDimension(ServerPlayerEntity playerEntity, ServerWorld world, String dimension, int x, int y, int z) {
+        playerEntity.teleport(world, x, y, z, playerEntity.getYaw(), playerEntity.getPitch());
+        EssentialAddonsUtils.sendToActionBar(playerEntity, "§6You have been teleported to " + x + " " + z + " in the §a" + dimension);
     }
 }
