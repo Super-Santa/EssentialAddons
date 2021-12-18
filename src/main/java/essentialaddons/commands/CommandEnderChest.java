ec1fa59c8234b28e47dc6d35bda274064095a690
Nick-Danut <55299148+Nick-Danut@users.noreply.github.com>
2021-12-17 19:34:47 -0500

    added alias to commandEnderChest

--- src/main/java/essentialaddons/commands/CommandEnderChest.java
+++ src/main/java/essentialaddons/commands/CommandEnderChest.java
@@ -2,6 +2,7 @@ package essentialaddons.commands;
 
 import carpet.settings.SettingsManager;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import essentialaddons.EssentialAddonsSettings;
 import net.minecraft.inventory.EnderChestInventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
@@ -14,12 +15,17 @@ import static net.minecraft.server.command.CommandManager.literal;
 
 public class CommandEnderChest {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(literal("enderchest").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandEnderChest))
-                .executes(context -> {
-                    ServerPlayerEntity playerEntity = context.getSource().getPlayer();
-                    EnderChestInventory enderChest = playerEntity.getEnderChestInventory();
-                    playerEntity.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInv, player) -> GenericContainerScreenHandler.createGeneric9x3(syncId, playerInv, enderChest), new TranslatableText("container.enderchest")));
-                    return 0;
-                }));
+
+        // Can't use redirect here because of this: https://github.com/Mojang/brigadier/issues/46
+
+        dispatcher.register(literal("enderchest").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandEnderChest)).executes(context -> CommandEnderChest.execute(context.getSource())));
+        dispatcher.register(literal("ec").requires((player) -> SettingsManager.canUseCommand(player, EssentialAddonsSettings.commandEnderChest)).executes(context -> CommandEnderChest.execute(context.getSource())));
+    }
+
+    private static int execute(ServerCommandSource source) throws CommandSyntaxException {
+        ServerPlayerEntity playerEntity = source.getPlayer();
+        EnderChestInventory enderChest = playerEntity.getEnderChestInventory();
+        playerEntity.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInv, player) -> GenericContainerScreenHandler.createGeneric9x3(syncId, playerInv, enderChest), new TranslatableText("container.enderchest")));
+        return 0;
     }
 }
