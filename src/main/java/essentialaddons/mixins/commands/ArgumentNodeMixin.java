edf5a6d38549a56291ce6d55ec437af64b863538
Sensei <wellsm488@gmail.com>
2022-07-01 23:37:38 +0100

    Fix ArgumentNodeMixin

--- src/main/java/essentialaddons/mixins/commands/ArgumentNodeMixin.java
+++ src/main/java/essentialaddons/mixins/commands/ArgumentNodeMixin.java
@@ -1,20 +1,24 @@
 package essentialaddons.mixins.commands;
 
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import essentialaddons.utils.EnumArgumentType;
+import net.minecraft.command.argument.serialize.ArgumentSerializer;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.registry.Registry;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net.minecraft.network.packet.s2c.play.CommandTreeS2CPacket$ArgumentNode")
 public class ArgumentNodeMixin {
-	@Redirect(method = "write(Lnet/minecraft/network/PacketByteBuf;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;Lnet/minecraft/command/argument/serialize/ArgumentSerializer$ArgumentTypeProperties;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/registry/Registry;getRawId(Ljava/lang/Object;)I"))
-	private static <T> int onGetRawId(Registry<T> instance, @Nullable T t) {
-		if (t instanceof EnumArgumentType.Serializer) {
-			return Registry.COMMAND_ARGUMENT_TYPE.getRawId(ArgumentTypesAccessor.getClassMap().get(StringArgumentType.class));
+	@Inject(method = "write(Lnet/minecraft/network/PacketByteBuf;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;Lnet/minecraft/command/argument/serialize/ArgumentSerializer$ArgumentTypeProperties;)V", at = @At("HEAD"), cancellable = true)
+	private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> void write(PacketByteBuf buf, ArgumentSerializer<A, T> serializer, T properties, CallbackInfo ci) {
+		if (serializer instanceof EnumArgumentType.Serializer) {
+			buf.writeVarInt(Registry.COMMAND_ARGUMENT_TYPE.getRawId(ArgumentTypesAccessor.getClassMap().get(StringArgumentType.class)));
+			serializer.writePacket(properties, buf);
+			ci.cancel();
 		}
-		return instance.getRawId(t);
 	}
 }
