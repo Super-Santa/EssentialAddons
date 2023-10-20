1613f8d9070d4b0d659d0a0528370a510abd399d
senseiwells <wellsm488@gmail.com>
2023-10-21 00:36:52 +0100

    Update rule generator

--- src/main/java/essentialaddons/utils/EssentialRuleGenerator.java
+++ src/main/java/essentialaddons/utils/EssentialRuleGenerator.java
@@ -28,8 +28,11 @@ public class EssentialRuleGenerator implements DedicatedServerModInitializer {
 	
 	[![Discord](https://badgen.net/discord/online-members/gn99m4QRY4?icon=discord&label=Discord&list=what)](https://discord.gg/gn99m4QRY4)
 	[![GitHub downloads](https://img.shields.io/github/downloads/super-santa/essentialaddons/total?label=Github%20downloads&logo=github)](https://github.com/Super-Santa/EssentialAddons/releases)
+	[![Modrinth downloads](https://img.shields.io/modrinth/dt/EssentialAddons?label=Modrinth%20downloads&logo=modrinth)](https://modrinth.com/mod/essentialaddons)
 	
 	[Fabric Carpet](https://github.com/gnembon/fabric-carpet) extension that adds things from the Spigot plugin Essentials, or other features I think are needed for Minecraft.
+	
+	## !!! Find Updated Releases on [Modrinth](https://modrinth.com/mod/essentialaddons) !!!
 	
 	Features can be enabled through the `/carpet` command:
 	```
