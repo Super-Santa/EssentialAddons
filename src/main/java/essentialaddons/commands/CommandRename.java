647f6d0923c3e24fda1ae64c5f6571f3e096d844
senseiwells <wellsm488@gmail.com>
2024-01-30 23:52:38 +0000

    Fixes #68 better late then never xD

--- src/main/java/essentialaddons/commands/CommandRename.java
+++ src/main/java/essentialaddons/commands/CommandRename.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import essentialaddons.EssentialSettings;
 import essentialaddons.EssentialUtils;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.argument.TextArgumentType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -18,6 +19,11 @@ public class CommandRename {
     private static final SimpleCommandExceptionType ITEM_IS_AIR = new SimpleCommandExceptionType(EssentialUtils.literal("Cannot rename air!"));
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        // We let them handle /rename command
+        if (FabricLoader.getInstance().isModLoaded("itemrename")) {
+            return;
+        }
+
         dispatcher.register(literal("rename").requires(enabled(() -> EssentialSettings.commandRename, "essentialaddons.command.rename"))
             .then(argument("name", TextArgumentType.text())
                 .executes(context -> {
