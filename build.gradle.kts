0608ed2a986b8410cc0f7dc4cf0692eb717328d2
senseiwells <wellsm488@gmail.com>
2025-07-13 08:24:32 +0800

    chore: Update fabric permissions

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val modVersion = "2.0.1"
+val modVersion = "2.0.2"
 val releaseVersion = "${modVersion}+${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.supersanta"
